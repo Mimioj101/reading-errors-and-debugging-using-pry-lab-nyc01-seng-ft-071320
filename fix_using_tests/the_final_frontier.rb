248b2ce39775a1ee12fe9cc3f0c8e79a5bb858ec
Mimi Ojserkis <mimioj@gmail.com>
2020-07-01 20:27:55 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,15 +1,19 @@
 # don't forget to add: require 'pry'
+require 'pry'
+
+
 
 def generate_star_date
   star_date = (rand(100000) + 400000) / 10.0
+binding.pry
 end
 
 
-# def state_log(star_date)
-#   "Captain's Log, star date #{star_date}."
-# end
+def state_log(star_date)
+  "Captain's Log, star date #{star_date}."
+end
 
-# def engage
-#   date = generate_star_date
-#   puts state_log(star_date)
-# end
+def engage
+  date = generate_star_date
+  puts state_log(star_date)
+end
